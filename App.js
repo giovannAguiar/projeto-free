446c5d2c7dd16bc951d7f52add79cc2c53ef9343
giovannAguiar <castilliag@gmail.com>
2025-03-26 10:40:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './slyle.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h3>giovanna de aguiar</h3> <br></br>
+        <img src='https://i.pinimg.com/736x/6e/8f/fb/6e8ffba5b4dc0f0ddacf611fc5060e0d.jpg'></img> <br></br>
+        <p>sobre:
+          sou a gigi, tenho 17 anos e faço desenvolvimento
+          de sistemas no senai
+        </p>
+      </main>
+    </div>
+  );
+}
+export default App;
